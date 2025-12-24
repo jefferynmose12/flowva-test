2ddba13a557c49604fa62fe2c152a634ad9064e6
Jeffery nmose <jefferyjohnnmose@gmail.com>
2025-12-24 17:04:44 +0100

    updated

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -25,7 +25,7 @@ const Button = ({
       className={`w-full h-12 lg:h-13.75 gap-2 flex justify-center text-base items-center p-2.75 text-center bg-[#9013FE] text-white font-medium border-none transition-colors ease-linear duration-200 rounded-[100px] hover:bg-[#6D28D9] ${
         disabled || loading
           ? "opacity-60 cursor-not-allowed hover:bg-gray-400"
-          : ""
+          : "cursor-pointer"
       } ${className}`}
     >
       {loading ? "Processing..." : children}
